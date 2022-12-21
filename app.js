9c456b8a0dc56e6d53f613ad0bef2a39e5276b41
olegMirkovskii <121171060+olegMirkovskii@users.noreply.github.com>
2022-12-21 19:40:17 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const  number = prompt('поставьте оценку 1 of 10');
+
+if (number > 10){
+    console.log('введите число от 1 до 10!!!')
+}else if (number > 5){
+    console.log("спасибо за выскую оценку");
+}else if (number < 5){
+    console.log("мы работает над улучшением сайта");
+}else isNaN(number)
+console.log('Введите ЧИСЛА!!!!')
+
+
